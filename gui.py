8d854c9142a6ec231dfa9f3f63ad7056c076b27c
OuttaSpace <outtaspace888@gmail.com>
2021-05-30 21:34:32 +0200

    Improved text generating and placing functions

--- gui.py
+++ gui.py
@@ -2,21 +2,33 @@ import tkinter as tk
 from tkinter import Button, font
 from PIL import ImageTk as itk
 
-Y_VALUE = 450
-LEFT_X_VALUE = 150
-RIGHT_X_VALUE = 560
+# Coordinates for 2 buttons, side by side, from left to right
+# Giving this button formation the color name red to differenciate from other button groups
+RED_Y_VALUE = 450
+RED_LEFT_X_VALUE = 150
+RED_RIGHT_X_VALUE = 560
+
+# Heading Line (bold)
+header_y = 200
+
+# Default coordinates for screen message who are one-liners (single line)
+default_x = 445  # x-coordinate always stays the same no matter how many lines
+default_middle_y = 250  # called default/middle line because all one liners will be using this y-cord. It is also the middle line y-cord for multiple line msgs.
+
+# 2nd line (after one liner)
+bottom_y = 300
 
 
 def btn_clicked():
     print("Button Clicked")
 
 
-def welcome_page(canvas):
+def welcome_page():
     left_btn_msg = "Pc"
-    create_btn(left_btn_msg, LEFT_X_VALUE, Y_VALUE)
+    create_btn(left_btn_msg, RED_LEFT_X_VALUE, RED_Y_VALUE)
 
     right_btn_msg = "Input"
-    create_btn(right_btn_msg, RIGHT_X_VALUE, Y_VALUE)
+    create_btn(right_btn_msg, RED_RIGHT_X_VALUE, RED_Y_VALUE)
 
     canvas.create_text(
         461.5,
@@ -49,12 +61,12 @@ def welcome_page(canvas):
     )
 
 
-def all_pc_options(canvas):
+def all_pc_options():
     left_btn_msg = "Default"
-    create_btn(left_btn_msg, LEFT_X_VALUE, Y_VALUE)
+    create_btn(left_btn_msg, RED_LEFT_X_VALUE, RED_Y_VALUE)
 
     right_btn_msg = "Vocabulary"
-    create_btn(right_btn_msg, RIGHT_X_VALUE, Y_VALUE)
+    create_btn(right_btn_msg, RED_RIGHT_X_VALUE, RED_Y_VALUE)
 
     canvas.create_text(
         457.5,
@@ -65,84 +77,66 @@ def all_pc_options(canvas):
     )
 
 
-def personal_choice(canvas):
+def personal_choice():
     left_btn_msg = "Default"
-    create_btn(left_btn_msg, LEFT_X_VALUE, Y_VALUE)
+    create_btn(left_btn_msg, RED_LEFT_X_VALUE, RED_Y_VALUE)
 
     right_btn_msg = "Vocabulary"
-    create_btn(right_btn_msg, RIGHT_X_VALUE, Y_VALUE)
+    create_btn(right_btn_msg, RED_RIGHT_X_VALUE, RED_Y_VALUE)
 
-    canvas.create_text(
-        457.5,
-        162.5,
-        text="There are 2 options to choose from. For the default version just",
-        fill="#000000",
-        font=("RhodiumLibre-Regular", int(24.0)),
-    )
+    message = "There are 2 options to choose from. For the default version just"
+    screen_msg(message, default_x, header_y)
 
-    canvas.create_text(
-        455.0,
-        220.0,
-        text="enter the words of your choice. Vocabulary style will need a keyword ",
-        fill="#000000",
-        font=("RhodiumLibre-Regular", int(24.0)),
-    )
-    canvas.create_text(
-        463.5,
-        280.0,
-        text="followed by a value. The key/value pair will be displayed together.",
-        fill="#000000",
-        font=("RhodiumLibre-Regular", int(24.0)),
-    )
+    message = "enter the words of your choice. Vocabulary style will need a keyword"
+    screen_msg(message, default_x, default_middle_y)
+
+    message = "followed by a value. The key/value pair will be displayed together."
+    screen_msg(message, default_x, bottom_y)
 
 
-def if_correct_end(canvas):
+def if_correct_end():
     left_btn_msg = "Quit"
-    create_btn(left_btn_msg, LEFT_X_VALUE, Y_VALUE)
+    create_btn(left_btn_msg, RED_LEFT_X_VALUE, RED_Y_VALUE)
 
     right_btn_msg = "Continue"
-    create_btn(right_btn_msg, RIGHT_X_VALUE, Y_VALUE)
+    create_btn(right_btn_msg, RED_RIGHT_X_VALUE, RED_Y_VALUE)
 
-    canvas.create_text(
-        461.5,
-        115.5,
-        text="Congratulations!",
-        fill="#000000",
-        font=("RhodiumLibre-Regular", int(28.0), "bold"),
-    )
-    canvas.create_text(
-        457.5,
-        225.0,
-        text="Your memory is impeccable! Would you like to play again?",
-        fill="#000000",
-        font=("RhodiumLibre-Regular", int(24.0)),
-    )
+    message = "Congratulations!"
+    screen_msg(message, default_x, header_y)
+
+    message = "Your memory is impeccable! Would you like to play again?"
+    screen_msg(message, default_x, default_middle_y)
 
 
-def ask_to_save(canvas):
+def ask_to_save():
     left_btn_msg = "No"
-    create_btn(left_btn_msg, LEFT_X_VALUE, Y_VALUE)
+    create_btn(left_btn_msg, RED_LEFT_X_VALUE, RED_Y_VALUE)
 
     right_btn_msg = "Yes"
-    create_btn(right_btn_msg, RIGHT_X_VALUE, Y_VALUE)
+    create_btn(right_btn_msg, RED_RIGHT_X_VALUE, RED_Y_VALUE)
 
-    canvas.create_text(
-        445,
-        179.0,
-        text="Would you like to save your input list?",
-        fill="#000000",
-        font=("RhodiumLibre-Regular", int(28.0)),
-    )
+    message = "Would you like to save your input list?"
+    screen_msg(message, default_x, default_middle_y)
 
 
-def create_btn(button_text, x_value, y_value):
+def create_btn(button_text, x_value, RED_y_value):
     btn = tk.Button(
         text=button_text,
         font=("RhodiumLibre-Regular", int(24.0)),
         bg="grey",
         activebackground="red",
     )
-    btn.place(x=x_value, y=y_value, width=180, height=80)
+    btn.place(x=x_value, y=RED_y_value, width=180, height=80)
+
+
+def screen_msg(message, x_value, y_value):
+    canvas.create_text(
+        x_value,
+        y_value,
+        text=message,
+        fill="#000000",
+        font=("RhodiumLibre-Regular", int(28.0)),
+    )
 
 
 if __name__ == "__main__":
